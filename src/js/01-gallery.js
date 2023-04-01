3c375a92bd9c75e6eb0ee28e63e6ff2d6c44f780
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-04-01 18:25:04 +0200

    changes
    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,18 +7,18 @@ const galleryEl = document.querySelector(".gallery");
 
 for (const img of galleryItems) {
   const listEl = `<div class="gallery__item">
-    <a class="gallery__link" href=${img.original}>
+    <li class="gallery__link" href=${img.original}>
       <img
         class="gallery__image"
         src=${img.preview}
         data-source=${img.original}
         alt=${img.description}
       />
-    </a>
+    </li>
   </div>`;
   galleryEl.insertAdjacentHTML("beforeend", listEl);
 }
-const lightbox = new SimpleLightbox(".gallery a", {
+const lightbox = new SimpleLightbox(".gallery li", {
   captions: true,
   captionDelay: 250,
   captionsData: "alt",
