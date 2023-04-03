aa075744cbb5d437e90ab7178b024fdb4ff9621c
raffamieczkowski <raffa.mieczkowski@gmail.com>
2023-04-03 18:18:41 +0200

    hw 8 done
    hw 8 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const player = new Player("vimeo-player");
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime).catch((error) => {
+    console.error(error);
+  });
+}
+player.on(
+  "timeupdate",
+  throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import { throttle } from 'lodash-es';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+const localStorageKey = 'feedback-form-state';
+
+// Load form state from localStorage on page load
+window.addEventListener('load', () => {
+  const formState = JSON.parse(localStorage.getItem(localStorageKey));
+  if (formState) {
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+});
+
+// Save form state to localStorage on every input change
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(formState));
+  }, 500)
+);
+
+// Clear form and localStorage on form submit
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Form submitted with data:', {
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+  form.reset();
+  localStorage.removeItem(localStorageKey);
+});
